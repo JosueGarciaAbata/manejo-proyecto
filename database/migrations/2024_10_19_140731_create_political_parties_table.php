c1e73178318d5a22e88334601d44de74006c7e91
DavidJosueP2 <deividjosue52@gmail.com>
2024-10-19 09:19:07 -0500

    Schema for Political Parties

--- database/migrations/2024_10_19_140731_create_political_parties_table.php
+++ database/migrations/2024_10_19_140731_create_political_parties_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,9 +13,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('political_parties', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_lis'); // Primary Key
+            $table->string('nom_lis', 100)->notNullable();
+            $table->text('des_lis')->nullable();
             $table->timestamps();
         });
+
+        DB::statement("ALTER TABLE <nombre_tabla> ADD CONSTRAINT check_nom_lis_alnum CHECK (nom_lis ~* '^[a-zA-Z0-9 ]+$')");            
     }
 
     /**
