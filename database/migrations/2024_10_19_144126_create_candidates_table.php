eb3814e89555af03395b230344be874ce22aca12
DavidJosueP2 <deividjosue52@gmail.com>
2024-10-19 09:58:09 -0500

    Schema for Candidates

---
+++ database/migrations/2024_10_19_144126_create_candidates_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id('id_can'); // Primary key
+            $table->string('nom_can', 100)->notNullable();
+            $table->string('ape_can', 100)->notNullable();
+            
+            $table->enum('car_can', ['Rector', 'Vicerrector'])->notNullable();
+            
+            $table->string('tit_can', 150)->notNullable();
+            $table->date('fec_ing_can')->notNullable();
+            
+            $table->unsignedBigInteger('id_pol_par_bel')->notNullable();
+            
+            $table->foreign('id_pol_par_bel')
+                ->references('id_lis')
+                ->on('political_parties')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
