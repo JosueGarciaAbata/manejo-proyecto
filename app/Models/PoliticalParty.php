aecd3b2620bc2386bb47fb2d07b324be1504a12f
DavidJosueP2 <deividjosue52@gmail.com>
2024-10-19 11:25:09 -0500

    Initial Political Party model

---
+++ app/Models/PoliticalParty.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PoliticalParty extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'political_parties'; 
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::deleting(function ($party) {
+            if ($party->candidates()->count() > 0) {
+                throw new \Exception("Cannot delete a party with associated candidates.");
+            }
+        });
+
+        static::creating(function ($party) {
+            if (!preg_match('/^[a-zA-Z0-9 ]+$/', $party->name)) {
+                throw new \Exception("The name must contain only alphanumeric characters and spaces.");
+            }
+        });
+    }
+}
