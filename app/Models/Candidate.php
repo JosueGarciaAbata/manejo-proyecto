294b897c4f12989529726c8e401b50b9b0703d6c
DavidJosueP2 <deividjosue52@gmail.com>
2024-10-19 11:43:01 -0500

    Initial Candidate  model

---
+++ app/Models/Candidate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Candidate extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'candidates'; 
+    protected $primaryKey = 'id_can';
+
+    protected $fillable = [
+        'nom_can',
+        'ape_can',
+        'car_can',
+        'tit_can',
+        'fec_ing_can',
+        'id_pol_par_bel', 
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($candidate) {
+            if (!PoliticalParty::find($candidate->id_pol_par_bel)) {
+                throw new \Exception("Invalid political party ID.");
+            }
+        });
+    }
+
+    public function politicalParty()
+    {
+        return $this->belongsTo(PoliticalParty::class, 'id_pol_par_bel');
+    }
+}
