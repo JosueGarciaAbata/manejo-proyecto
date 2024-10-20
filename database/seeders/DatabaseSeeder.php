f79a70fad940c9fba6c3c598513a674bc46b0e86
DavidJosueP2 <deividjosue52@gmail.com>
2024-10-20 09:01:39 -0500

    Seeder example Proposal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PoliticalPartiesSeeder::class,
             CandidateSeeder::class,
+            ProposalSeeder::class,
         ]);
 
         // User::factory(10)->create();
