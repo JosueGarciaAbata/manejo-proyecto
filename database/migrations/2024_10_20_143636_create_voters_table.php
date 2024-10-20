0824a6e405ce9cb50604c116df247659f08f6230
DRTX <davidmanjarres2004@gmail.com>
2024-10-20 10:26:38 -0500

    Adding migrations

---
+++ database/migrations/2024_10_20_143636_create_voters_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voters', function (Blueprint $table) {
+            $table->id('id_vot');
+            $table->string('nom_vot', 25);
+            $table->string('ape_vot', 25);
+            $table->string('ema_vot')->unique();
+            $table->foreignId('id_lis_vot')
+                  ->constrained('political_parties', 'id_lis')
+                  ->onDelete("cascade");
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('voters');
+    }
+};
---
+++ database/migrations/2024_10_20_143744_create_events_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('id_eve');
+            $table->string('tit_eve', 20);
+            $table->string('des_eve', 150);
+            $table->dateTime("fec_pub_eve");
+            $table->dateTime("fec_eve");
+            $table->string("tag_eve", 50)->notNullable();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
