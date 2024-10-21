3df2fd855482a28dbf501002a8041f6686037342
DavidJosueP2 <deividjosue52@gmail.com>
2024-10-20 21:27:39 -0500

    Add events Route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
 use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('pages.home');
-});
+})->name('home');
 
 Route::get('/candidates', [CandidateController::class, 'show']);
 
-Route::get('/eventsAndNews', function () {
-    return view('pages.eventsAndNews');
-});
+Route::get('/eventsAndNews', [EventController::class, 'index']);
 
 Route::get('/proposals', function () {
     return view('pages.proposals');
