a80adc1afffbf518bbfca21de4d1a1cae0844089
DavidJosueP2 <deividjosue52@gmail.com>
2024-10-22 06:57:20 -0500

    Add dates information

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Event extends Model
 {
@@ -11,18 +12,20 @@ class Event extends Model
 
     protected $table = 'events'; 
     protected $primaryKey = 'id_eve';
-
+    
     protected $fillable = [
         'tit_eve',
         'des_eve',
         'fec_pub_eve',
-        'fec_eve',
+        'fec_ini_eve',
+        'fec_fin_eve',
         'tag_eve',
         'pre_img',
-        'res_img'
+        'res_img',
+        'dir_eve'
     ];
-
-    protected $dates = ['deleted_at'];
+    
+    protected $dates = ['deleted_at', 'fec_pub_eve', 'fec_ini_eve', 'fec_fin_eve'];
     
     public function getPreviewImgUrlAttribute()
     {
@@ -31,7 +34,7 @@ class Event extends Model
 
         return asset('assets/images/event/example_preview_event.jpg');
     }
-
+    
     public function getResourceImgUrlAttribute()
     {
         $path = public_path($this->res_img);
@@ -42,5 +45,24 @@ class Event extends Model
         
         return asset('assets/images/resources/example_event.jpg');
     }
-
+    
+    public function getFormattedStartDateAttribute()
+    {
+        return Carbon::parse($this->fec_ini_eve)->format('d F, Y h:i A');
+    }
+    
+    public function getFormattedEndDateAttribute()
+    {
+        return Carbon::parse($this->fec_fin_eve)->format('d F, Y h:i A');
+    }
+    
+    public function getStartTimeAttribute()
+    {
+        return Carbon::parse($this->fec_ini_eve)->format('h:i A');
+    }
+    
+    public function getEndTimeAttribute()
+    {
+        return Carbon::parse($this->fec_fin_eve)->format('h:i A');
+    }
 }
