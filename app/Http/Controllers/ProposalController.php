58a5df177ecd10ffcc622793f308acbc140cd10b
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-10-23 20:15:51 -0500

    Add basic controller

--- app/Http/Controllers/ProposalController.php
+++ app/Http/Controllers/ProposalController.php
@@ -3,8 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Proposal;
 
 class ProposalController extends Controller
 {
-    //
+    public $numProposal = 5;
+    public function index()
+    {
+        $proposals = Proposal::paginate(6);
+        return view('pages.proposals', compact('proposals'));
+    }
+
+    public function show($id)
+    {
+        $proposal = Proposal::findOrFail($id);
+        return view('pages.proposal-detail', compact('proposal'));
+    }
+
+    public function search(Request $request)
+    {
+        $request->validate([
+            'date' => 'nullable|date',
+            'query' => 'nullable|string',
+        ]);
+
+        $query = Proposal::query();
+
+        if ($request->filled('date')) {
+            $query->whereDate('fec_ini_pro', $request->input('date'));
+        }
+
+        if ($request->filled('query')) {
+            $query->where(function ($q) use ($request) {
+                $q->where('tit_pro', 'LIKE', "%{$request->input('query')}%")
+                    ->orWhere('tag_pro', 'LIKE', "%{$request->input('query')}%");
+            });
+        }
+
+        $Proposals = $query->paginate(6);
+
+        return view('pages.proposals', compact('proposals'));
+    }
+
+    public function searchByDate(Request $request)
+    {
+        $request->validate([
+            'date' => 'required|date',
+        ]);
+
+        $proposals = Proposal::whereDate('fec_ini_pro', $request->date)->paginate(6);
+        return view('pages.proposals', compact('proposals'));
+    }
+
+    public function searchByTag(Request $request)
+    {
+        $request->validate([
+            'tag' => 'required|string',
+        ]);
+
+        $proposals = Proposal::where('tag_pro', 'LIKE', "%{$request->tag}%")->paginate(6);
+        return view('pages.proposals', compact('proposals'));
+    }
+
+    public function latestProposals()
+    {
+        return Proposal::orderBy('fec_ini_pro', 'desc')->take(3)->get();
+    }
+
 }
