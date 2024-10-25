9820d945efdf6e52eefe7b36fbbfdfc03b8d4cce
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-10-25 17:50:59 -0500

    Poll voting function

--- resources/views/pages/poll.blade.php
+++ resources/views/pages/poll.blade.php
@@ -19,21 +19,19 @@
         </div>
     </section>
 
-    <!--Reuso de css-->
+    <!-- Reuso de CSS -->
     <section class="event-details">
         <div class="container">
             <div class="row">
-                @foreach ($partyVotes['parties'] as $party)
-                    <div class="col-lg-6 wow fadeInLeft animated same-height"
-                        style="visibility: visible; animation-name: fadeInLeft;">
-                        <button id="{{ $party }}" type="button" class="btn btn-primary btn-lg">
-                            {{ $party }}
-                        </button>
-                    </div>
-                @endforeach
+                <form action="{{ route('home') }}" method="GET">
+                    @foreach($partyVotes['parties'] as $party)
+                    
+                    @endforeach
+                </form>
             </div>
         </div>
     </section>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         const ctx = document.getElementById('polls');
@@ -45,7 +43,6 @@
                 backgroundColor: [
                     'rgb(255, 99, 132)',
                     'rgb(54, 162, 235)',
-                    'rgb(255, 205, 86)'
                 ],
                 hoverOffset: 4
             }]
@@ -53,6 +50,64 @@
         const chart = new Chart(ctx, {
             type: 'doughnut',
             data: datos
+        });
+    </script>
+
+    <!-- Modal para agregar votos -->
+    <section id="container-add-vote" class="modal">
+        <article class="contenido-modal">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section-title">
+                            <h2>Tu voto es importante</h2>
+                            <p>Elige con responsabilidad, integridad y honestidad</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-9">
+                        <form action="{{ route('vote.store') }}" method="POST" class="mailchimp-one__form add-vote">
+                            @csrf
+                            <input type="hidden" name="id_can" id="id_can" value="">
+                            <input type="text" name="ema_vot" placeholder="Email" required>
+                            <button type="submit" class="thm-btn mailchimp-one__form-btn">Enviar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <span class="cerrar">&times;</span>
+        </article>
+    </section>
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const modal = document.getElementById("container-add-vote");
+            const closeBtn = document.querySelector(".cerrar");
+            const formModal = document.getElementById("form-add-vote");
+
+            const candidates = document.querySelectorAll(".candidate-item");
+
+            const closeModal = modalContainer => {
+                if (modalContainer) modalContainer.style.display = "none";
+            };
+
+            closeBtn.addEventListener("click", (e) => {
+                const modalContainer = closeBtn.closest(".modal");
+                closeModal(modalContainer);
+            });
+
+            candidates.forEach(candidate => {
+                candidate.addEventListener("click", () => modal.style.display = "block");
+                const candidateId = candidate.querySelector(".cd-pic").id;
+                document.getElementById("id_can").value = candidateId;
+            });
+
+            window.addEventListener("click", (event) => {
+                if (event.target.classList.contains("modal")) {
+                    const modals = document.querySelectorAll(".modal");
+                    modals.forEach(modal => closeModal(modal));
+                }
+            });
         });
     </script>
 @endsection
