d07a8966176420500ebe363452db6c2c562b5d0b
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-10-24 22:39:00 -0500

    Add basic poll view

--- resources/views/pages/poll.blade.php
+++ resources/views/pages/poll.blade.php
@@ -3,9 +3,42 @@
 @section('title', 'Votaciones')
 
 @section('content')
-    <script src="{{ asset('assets/js/deleteme.js') }}"></script>
-    
-    <div style="width: 100%">
-        <canva id="polls"></canva>
+
+    <section class="inner-banner">
+        <div class="container">
+            <h2 class="inner-banner__title">Vota Ahora!</h2><!-- /.inner-banner_title -->
+            <ul class="list-unstyled thm-breadcrumb">
+                <li><a href="{{ route('home') }}">Home</a></li>
+            </ul><!-- /.list-unstyled -->
+        </div><!-- /.container -->
+    </section>
+    <div>
+        <canvas id="polls"></canvas>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        const ctx = document.getElementById('polls');
+        const datos = {
+            labels: [
+                'Red',
+                'Blue',
+                'Yellow'
+            ],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+            }]
+        };
+        const chart = new Chart(ctx, {
+            type: 'doughnut',
+            data: datos
+        });
+    </script>
 @endsection
