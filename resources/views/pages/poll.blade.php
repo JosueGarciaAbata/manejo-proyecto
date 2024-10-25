40769ce76536de06df8538f68ce4f7cf4642532a
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-10-24 23:48:54 -0500

    Add poll donut chart

--- resources/views/pages/poll.blade.php
+++ resources/views/pages/poll.blade.php
@@ -6,28 +6,42 @@
 
     <section class="inner-banner">
         <div class="container">
-            <h2 class="inner-banner__title">Vota Ahora!</h2><!-- /.inner-banner_title -->
+            <h2 class="inner-banner__title">Tu voto es importante!</h2><!-- /.inner-banner_title -->
             <ul class="list-unstyled thm-breadcrumb">
                 <li><a href="{{ route('home') }}">Home</a></li>
             </ul><!-- /.list-unstyled -->
         </div><!-- /.container -->
     </section>
-    <div>
-        <canvas id="polls"></canvas>
-    </div>
 
+    <section class="poll-results">
+        <div class="container">
+            <canvas id="polls"></canvas>
+        </div>
+    </section>
+
+    <!--Reuso de css-->
+    <section class="event-details">
+        <div class="container">
+            <div class="row">
+                @foreach ($partyVotes['parties'] as $party)
+                    <div class="col-lg-6 wow fadeInLeft animated same-height"
+                        style="visibility: visible; animation-name: fadeInLeft;">
+                        <button id="{{ $party }}" type="button" class="btn btn-primary btn-lg">
+                            {{ $party }}
+                        </button>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         const ctx = document.getElementById('polls');
         const datos = {
-            labels: [
-                'Red',
-                'Blue',
-                'Yellow'
-            ],
+            labels: {!! json_encode($partyVotes['parties']) !!},
             datasets: [{
-                label: 'My First Dataset',
-                data: [300, 50, 100],
+                label: 'Votos',
+                data: {!! json_encode($partyVotes['votes']) !!},
                 backgroundColor: [
                     'rgb(255, 99, 132)',
                     'rgb(54, 162, 235)',
