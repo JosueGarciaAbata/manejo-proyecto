ce403ed6d707902e6e72a377f875a4a78786b6b5
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-10-28 20:07:21 -0500

    Add basic mail controller

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\MailSuggestion;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    function sendEmail() {
+        $to = "jamesrnewman3125@gmail.com";
+        $msg = "Hola causa";
+        $sbj = "La wea penca";
+        Mail::to($to)->send(new MailSuggestion($msg,$sbj));
+    }
+
+}
