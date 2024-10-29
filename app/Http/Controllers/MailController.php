997125e62f097c18c5b4df0aa59105aa1a8b565b
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-10-28 21:18:35 -0500

    Add email validation on mail Controller

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Mail\MailSuggestion;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    function sendEmail() {
-        $to = "mateoparedeskt1800@gmail.com";
-        $msg = "Hola causa";
-        $sbj = "La wea penca";
+    function sendEmail($email) {
+        if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+            return view('errors.404');
+        }
+        $to = $email;
+        $msg = "Apreciamos tu voto";
+        $sbj = "Registro de voto";
         Mail::to($to)->send(new MailSuggestion($msg,$sbj));
     }
 
