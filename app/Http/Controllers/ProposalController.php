89f50943bbb6d87682f4b80822cc8ed367e5f811
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-12-07 18:21:07 -0500

    Modify ProposalController to create posts

--- app/Http/Controllers/ProposalController.php
+++ app/Http/Controllers/ProposalController.php
@@ -70,4 +70,29 @@ class ProposalController extends Controller
     {
         return Proposal::orderBy('fec_inc_pro', 'desc')->take(3)->get();
     }
+
+    public function create(Request $request, $candidateId) {
+        $candidate = Candidate::findOrFail($candidateId);
+        
+        $request->validate([
+            'tit_pro' => 'required',
+            'des_pro' => 'required',
+            'fec_inc_pro' => 'required'
+        ]);
+
+        $propuesta = new Proposal();
+        $propuesta->tit_pro = $request->tit_pro;
+        $propuesta->des_pro = $request->des_pro;
+        $propuesta->fec_inc_pro = $request->fec_inc_pro;
+
+        if ($candidate->proposals()->save($propuesta)) {
+            return response()->json([
+                'msg' => 'Propuesta registrada'
+            ]);
+        } else {
+            return response()->json([
+                'msg' => 'Problema al registrar la propuesta'
+            ]);
+        }
+    }
 }
--- app/Models/Proposal.php
+++ app/Models/Proposal.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Proposal extends Model
 {
     use SoftDeletes;
+    use HasFactory;
 
     protected $table = 'proposals';
     protected $primaryKey = 'id_pro'; 
