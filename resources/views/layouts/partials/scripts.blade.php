1cbf6ded00c5bc768a61328f02f368a6cc123450
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-12-07 23:16:23 -0500

    Add view to create a new proposal

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -19,3 +19,7 @@
 <script src="{{ asset('assets/js/startCountDown.js') }}"></script>
 <!-- Template scripts -->
 <script src="{{ asset('assets/js/theme.js') }}"></script>
+
+<!--Tagify Scripts-->
+<script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify"></script>
+<script src="https://cdn.jsdelivr.net/npm/@yaireo/tagify/dist/tagify.polyfills.min.js"></script>
--- resources/views/pages/proposals/add-proposal.blade.php
+++ resources/views/pages/proposals/add-proposal.blade.php
@@ -3,20 +3,46 @@
 @section('title','Create Proposal')
 
 @section('content')
-    <div class="col-md-3">
-        <form action="{{ route('admin.proposals.create') }}" method="post" id="addProposal" enctype="multipart/form-data" class="mailchimp-one__form add-suggestion">
-            @csrf
-                <input type="text" name="tit_pro" id="tit_pro" placeholder="Titulo de la propuesta">
-                <input type="date" name="fec_inc_pro" id="fec_inc_pro" >
-                <textarea maxlength="150" name="des_pro" id="des_pro" placeholder="Descripcion"></textarea>
-                <input type="text" name="tags_pro" id="tags_pro" placeholder="Tags de su pagina">
-                <select class="form-select" name="id_can" id="id_can">
-                    <option value="">Seleccione un candidato</option>
-                    @foreach(\App\Models\Candidate::all() as $candidate)
-                    <option value="{{ $candidate->id_can }}">{{ $candidate->nom_can }} {{ $candidate->ape_can }}</option>
-                    @endforeach
-                </select>
-                <button type="submit" class="thm-btn mailchimp-one__form-btn">Registrar propuesta</button>
-        </form>
+
+    <div class="container">
+        <div class="inner-container thm-white-bg">
+            <div class="row align-items-center">
+                <h1>Cree una nueva propuesta</h1>
+            </div>
+            <form action="{{ route('admin.proposals.create') }}" method="post" id="addProposal" enctype="multipart/form-data" class="mailchimp-one__form add-suggestion">
+                @csrf
+                <div class="row">
+                    <input type="text" name="tit_pro" id="tit_pro" placeholder="Titulo de la propuesta">
+                </div>
+                <div class="row">
+                    <input type="date" name="fec_inc_pro" id="fec_inc_pro" >
+                </div>
+                <div class="row">
+                    <textarea maxlength="150" name="des_pro" id="des_pro" placeholder="Descripcion"></textarea>
+                </div>
+                <br>
+                <div class="row">
+                    <input type="text" name="tags_pro" id="tags_pro" placeholder="Tags">
+                </div>
+                <br>
+                <div class="row">
+                    <select class="form-select form-select-lg mb-3" name="id_can" id="id_can">
+                        <option selected>Seleccione un candidato</option>
+                        @foreach(\App\Models\Candidate::all() as $candidate)
+                        <option value="{{ $candidate->id_can }}">{{ $candidate->nom_can }} {{ $candidate->ape_can }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="row">
+                    <button type="submit" class="thm-btn mailchimp-one__form-btn">Registrar propuesta</button>
+                </div>    
+            </form>
+        </div>
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const input = document.getElementById('tags_pro')
+            const tagify = new Tagify(input)
+        });
+    </script>
 @endsection
