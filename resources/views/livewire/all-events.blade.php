f9bb98787cc0f405f69ef48380bfde767534c8ec
DavidJosueP2 <deividjosue52@gmail.com>
2024-12-14 18:45:36 -0500

    show all events to an admin

---
+++ resources/views/livewire/all-events.blade.php
@@ -0,0 +1,38 @@
+<div>
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <label for="" class="form-label">Buscar</label>
+            <input type="text" class="form-control" placeholder="Keyword..." wire:model='search'>
+        </div>
+        
+        <div class="col-md-2 mb-3">
+            <label for="" class="form-label">Ordenar en</label>
+            <select class="form-select" wire:model='orderBy'>
+                <option value="asc">Asc</option>
+                <option value="desc">Desc</option>
+            </select>
+        </div>  
+    </div>
+
+    <div class="row row-cards">
+        @forelse ($events as $event)
+        <div class="col-md-4 col-lg-2">
+            <div class="card">
+                <img src="{{ $event->preview_img_url }}" alt="" class="card-img-top">
+                <div class="card-body p-2">
+                    <h3 class="m-0 mb-1">{{$event->tit_eve}}</h3>
+                </div>
+                <div class="d-flex">
+                    <a href="" class="card-btn">Edit</a>
+                    <a href="" wire:click.prevent='deletePost({{$event->id}})' class="card-btn">Delete</a>
+                </div>
+            </div>
+        </div>
+            
+        @empty
+         <span class="text-danger">No event(s) found</span>   
+        @endforelse
+    </div>
+    <div class="d-block mt-2">
+    {{ $events->links('livewire::simple-bootstrap') }}
+</div>
